5028f983bf9810e71a6aa40959f3c3488fec45e4
Yevhen Danilov <danilov.evgen@gmail.com>
2021-09-01 17:55:32 +0300

    start files

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+
+// Change code below this line
+
+console.log(galleryItems);
+  
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData:"alt" });
